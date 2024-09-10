1a51a41769c31d5033afb276e24d4ca0b78ae6d5
salahAhmed <salahkobia13@gmail.com>
2024-09-10 23:14:50 +0300

    create post job api route

--- models/Job.ts
+++ models/Job.ts
@@ -4,14 +4,15 @@ import mongoose, {Schema, model} from "mongoose";
 const JobSchema = new Schema({
     title: {type: String, require:true},
     description: { type: String, required: true },
+    jobType: {type:String, required: true},
+    jobCategory: {type: String, required:true},
     company: { type: String, required: true },
     location: { type: String, required: true },
-    salary: { type: Number, required: true },
-    postedBy: {type: Schema.Types.ObjectId, ref: 'User', require:true},
-    datePosted:{type:Date, default: Date.now}
-    
+    salary: { type: Number },
+    postedBy: {type: Schema.Types.ObjectId, ref: 'User'},
+    datePosted:{type:Date, default: Date.now}  
 })
 
-const Job = model('Job', JobSchema);
+const Job = mongoose.models.Job || model('Job', JobSchema);
 
 export default Job
---
+++ pages/api/postJob/postJob.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Job from "../../../models/Job";
+import dbConnect from "../../../lib/mongo";
+
+
+export default async function postJob(req: NextApiRequest, res:NextApiResponse){
+    if(req.method !== 'POST'){
+        res.status(405).json({message : "Methods Is Not Allowed"})
+    }
+    await dbConnect();
+    const {title, company, location, jobType, jobCategory, description, salary, userId} = req.body;
+    console.log(req.body);
+    if(!title || !company || !location || !jobType || !jobCategory || !description){
+        res.status(400).json({message: 'Please provide all required fields'});
+    }else{
+        try{
+            const newjob = new Job({
+                title,
+                company,
+                location,
+                jobType,
+                jobCategory,
+                description,
+                salary,
+                userId
+            });
+            await newjob.save() 
+            res.status(201).json({message: "job saved"})
+        }catch(error){
+            res.status(500).json({message: 'enternal server error'})
+        }
+    }
+}
