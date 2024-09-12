a036d2ed6d1ab8869bce85841b9385957450b2b3
salahAhmed <salahkobia13@gmail.com>
2024-09-12 16:01:07 +0300

    imporve design of job card

--- src/app/browseJobs/page.tsx
+++ src/app/browseJobs/page.tsx
@@ -3,16 +3,9 @@ import { jobs } from './jobs'
 import { FaCode } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaMoneyBillAlt } from "react-icons/fa";
-
-// title: "Graphic Designer",
-// description: "Create visually appealing graphics for marketing materials and websites.",
-// jobType: "Part Time",
-// jobCategory: "Design",
-// company: "Creative Studios",
-// location: "New York, NY",
-// salary: 75000,
-// postedBy: "AnotherObjectIdHere",
-// datePosted: new Date("2024-09-10"),
+import { FaRegClock } from "react-icons/fa";
+import SecondaryButton from "../components/ui/button/secondaryButton";
+import Link from "next/link";
 const BrowseJobs = () => {
     return (
         <div>
@@ -26,26 +19,32 @@ const BrowseJobs = () => {
                     <div className="container mx-auto">
                         <div className="job-list flex flex-col gap-4">
                             {jobs.map((job) => (
-                                <div className="job-card flex items-center p-3 bg-blue-100 rounded">
+                                <div className="job-card flex items-center p-3 bg-blue-50 rounded border border-transparent hover:border-green-400 duration-300 cursor-pointer">
                                     <div className="job-icon bg-gray-200 p-5 ">
                                          <FaCode className="text-3xl text-orange-600"/>
                                     </div>
                                     <div className="job-details ml-4 grow">
                                         <h3 className="job-title text-2xl mb-3 font-semibold text-darkBlue">{job.title}</h3>
-                                        <div className="flex ">
-                                            <div className="job-location flex items-center mr-4">
+                                        <div className="flex w-8/12 justify-between">
+                                            <div className="job-location flex items-center text-gray-500">
                                                 <FaLocationDot className="mr-1"/>
                                                 <span>{job.location}</span>
                                             </div>
-                                            <div className="job-salary flex items-center mx-4">
+                                            <div className="job-location flex items-center text-gray-500">
+                                                <FaRegClock className="mr-1"/>
+                                                <span>{job.jobType}</span>
+                                            </div>
+                                            <div className="job-salary flex items-center font-semibold px-3 py-1 bg-green-500 rounded text-white">
                                                 <FaMoneyBillAlt className="mr-1"/>
-                                                <span>{job.salary}</span>
+                                                <span>{job.salary} $</span>
                                             </div>
                                         </div>
                                     </div>
                                     <div>
                                         <div className="apply">
-                                            <a href="#">Apply Now</a>
+                                            <Link href = '/'>
+                                                <SecondaryButton title="Apply Now" />
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
